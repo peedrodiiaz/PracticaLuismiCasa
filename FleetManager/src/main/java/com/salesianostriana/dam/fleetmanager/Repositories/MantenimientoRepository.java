d91be2ef9660476a06ff83280c7a360ef3fd96ec
Pedro <diazpd2006>
2026-01-13 19:52:41 +0100

    subida

--- FleetManager/src/main/java/com/salesianostriana/dam/fleetmanager/Repositories/MantenimientoRepository.java
+++ FleetManager/src/main/java/com/salesianostriana/dam/fleetmanager/Repositories/MantenimientoRepository.java
@@ -2,6 +2,16 @@ package com.salesianostriana.dam.fleetmanager.Repositories;
 
 import com.salesianostriana.dam.fleetmanager.Models.Mantenimiento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MantenimientoRepository extends JpaRepository <Mantenimiento,Long> {
+    List<Mantenimiento> findMantenimientosByVehiculoId (Long vehiculoId);
+
+    @Query("select m from Mantenimiento m where m.vehiculo.id = ?1 order by m.fecha DESC LIMIT 1")
+    Optional<Mantenimiento> findFirstByVehiculo_IdOrderByFechaDesc(Long id);
+
+
 }
--- FleetManager/src/main/java/com/salesianostriana/dam/fleetmanager/Repositories/VehiculoRepository.java
+++ FleetManager/src/main/java/com/salesianostriana/dam/fleetmanager/Repositories/VehiculoRepository.java
@@ -1,7 +1,12 @@
 package com.salesianostriana.dam.fleetmanager.Repositories;
 
+import com.salesianostriana.dam.fleetmanager.Models.Mantenimiento;
 import com.salesianostriana.dam.fleetmanager.Models.Vehiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VehiculoRepository extends JpaRepository <Vehiculo,Long> {
+
+        List<Vehiculo> findByEstado(String estado);
 }
